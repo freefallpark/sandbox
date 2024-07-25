95506d1846c941d06fe86735707d76fa2b8217cb
pkyleEros <kyle.park@ultradent.com>
2024-07-25 07:24:05 -0600

    [prototype]: hello world program to try and include in a Yocto Image

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-#include <memory>
-
-void func(std::shared_ptr<int> d){
-    std::cout << "func: " << d << d.use_count() << std::endl;
-}
-void func(int* d){
-    std::cout << "func: " << d << std::endl;
-}
 
 int main() {
-    std::shared_ptr<int> sharedPtr_int(new int);
-    int *ptr_int;
-    std::cout << "main_shared: " << sharedPtr_int << std::endl;
-    func(sharedPtr_int);
-    std::cout << "main_ptr: " << ptr_int << std::endl;
-    func(ptr_int);
-    return 0;
+	std::cout << "Hello World" << std::endl;
+	return 0;
 };
